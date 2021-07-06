c82bc6df8bdd9046ebe2c6b9d7803f6675e145e0
hqg <qinggang.he@shunyagroup.com>
2021-07-06 15:28:30 +0800

    '注释'

--- transport/src/main/java/io/netty/channel/ChannelInitializer.java
+++ transport/src/main/java/io/netty/channel/ChannelInitializer.java
@@ -126,6 +126,7 @@ public abstract class ChannelInitializer<C extends Channel> extends ChannelInbou
     private boolean initChannel(ChannelHandlerContext ctx) throws Exception {
         if (initMap.add(ctx)) { // Guard against re-entrance.
             try {
+                // 调用我们重写的initChannel方法，把handler添加到pipeline链上
                 initChannel((C) ctx.channel());
             } catch (Throwable cause) {
                 // Explicitly call exceptionCaught(...) as we removed the handler before calling initChannel(...).
@@ -134,6 +135,7 @@ public abstract class ChannelInitializer<C extends Channel> extends ChannelInbou
             } finally {
                 ChannelPipeline pipeline = ctx.pipeline();
                 if (pipeline.context(this) != null) {
+                    // 把自己从pipeline中移除
                     pipeline.remove(this);
                 }
             }
